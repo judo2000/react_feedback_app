e349b3848683c92ce9b85f5c6837b4036cdd7dfb
Stephen Moore <judo2000@comcast.net>
2022-04-26 19:15:43 -0600

    Update Button component and add state updats to button in feedbackform

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,21 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("Text must be at least 10 characters");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
     setText(e.target.value);
   };
   return (
@@ -20,8 +33,12 @@ function FeedbackForm() {
             type='text'
             placeholder='Write a review'
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
